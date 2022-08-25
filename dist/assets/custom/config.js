5e9b1de7137516b11e0e3d6e0ec7eaea6790afc9
fantasy995 <1642063542@qq.com>
2022-08-25 19:15:35 +0800

    refactor: 现实组件化的首页顶部

---
+++ dist/assets/custom/config.js
@@ -0,0 +1,8 @@
+const max_file_items = 5;
+const user_name = 'Fantasy955';
+const real_name = 'WeiJL';
+const github = 'https://github.com/fantasy995'
+
+export default {
+    config: {max_file_items, user_name, real_name, github}
+}
