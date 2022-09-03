44f914685fee7ac41dee2b16f6830685dad0b251
fantasy995 <1642063542@qq.com>
2022-09-03 22:13:29 +0800

    底部声明

--- src/components/common/PageFooter.vue
+++ src/components/common/PageFooter.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="container mt-5">
     <footer class="bg-white border-top p-3 text-muted small">
-      <div class="row align-items-center justify-content-between">
-        <div>
-          <span class="navbar-brand mr-2"><strong>{{$globalParams.real_name}}</strong></span>
-          Copyright &copy;
-          {{year}}
-          . All rights reserved.
-        </div>
+      <div>
+        <span class="navbar-brand mr-2"
+          ><strong>{{ $globalParams.real_name }}</strong></span
+        >
+        Copyright &copy;
+        {{ year }}
+        . All rights reserved.
+      </div>
+      <div>
+        <span>Powered By</span>
+        <a
+          target="_blank"
+          class="text-secondary font-weight-bold ml-1"
+          href="https://cn.vuejs.org/"
+          >Vue 3</a
+        >
       </div>
     </footer>
   </div>
@@ -15,14 +24,26 @@
 
 <script>
 export default {
-    data() {
-        return {
-            year: new Date().getFullYear()
-        }
-    },
+  data() {
+    return {
+      year: new Date().getFullYear(),
+    };
+  },
 };
-
 </script>
 
-<style>
+<style scoped>
+footer {
+  display: flex;
+  justify-content: space-between;
+}
+
+footer div {
+  display: flex;
+  align-items: center;
+  justify-items: center;
+}
+.ml-1 {
+  margin-left: 8px;
+}
 </style>
