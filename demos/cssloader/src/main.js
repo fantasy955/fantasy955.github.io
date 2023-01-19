4e09ee07d9ffb974e6b13ba3deba88a3f3bedd72
weijl <1642063542@qq.com>
2023-01-19 22:43:16 +0800

    fix: remove unjumpable href attr

--- demos/cssloader/src/main.js
+++ demos/cssloader/src/main.js
@@ -3,3 +3,7 @@ import styles from "./index.css";
 
 console.log(styles);
 console.log([1,2,3].reduce((pre, cure) => pre+cure, 0));
+
+(0, _includes["default"])(array).call(array, function(item) {
+    return item > 2;
+})
--- update_blogs.bat
+++ update_blogs.bat
@@ -1,4 +1,5 @@
 python .\public\assets\blogs\genlist.py
 git add ./public/assets
+git add ./src/components/blog/menu.json
 git commit -m "blogs: update contens"
 git push origin dev
