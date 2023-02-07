ebacdeb7aa8f953d22d94de3873ee1ec9cebd2ab
weijl <1642063542@qq.com>
2023-02-07 22:18:38 +0800

    chore: 限制canvas的最大显示宽度

--- src/components/demo/fileandblob/ImageCanvas.vue
+++ src/components/demo/fileandblob/ImageCanvas.vue
@@ -2,7 +2,7 @@
     <div class="d-flex w-100 p-3 flex-wrap">
         <input type="file" class="btn" accept="image/png, image/jpeg" @change="handleFileChange" />
         <div class="d-block w-100 overflow-auto">
-            <canvas ref="canvas"></canvas>
+            <canvas ref="canvas" class="mw-100"></canvas>
         </div>
     </div>
 </template>
@@ -21,6 +21,7 @@ const handleFileChange = (e) => {
             // canvas.value.style.height = `${img.height}px`;
             canvas.value.width = img.width;
             canvas.value.height = img.height;
+            // 先是参考自己的本身画布大小进行绘制，绘制完毕，由style指定的大小，渲染在浏览器页面
             const context = canvas.value.getContext('2d');
             context.clearRect(0, 0, canvas.value.width, canvas.value.height);
             context.drawImage(img, 0, 0);
