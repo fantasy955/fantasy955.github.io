24b6f0ea6d14de48835e2ade11a21c9c000a97f5
weijl <1642063542@qq.com>
2023-02-08 13:23:34 +0800

    chore: 完善添加矩形功能

--- src/components/demo/fileandblob/ImageCanvas.vue
+++ src/components/demo/fileandblob/ImageCanvas.vue
@@ -2,20 +2,26 @@
     <div class="d-flex w-100 p-3 flex-wrap">
         <input type="file" ref="btnInput" class="btn" accept="image/png, image/jpeg" @change="handleFileChange" />
         <a class="btn btn-primary" ref="btnDownload" href="javascript:void(0)" @click="downloadCanvas">保存文件</a>
-        <div class="border w-100 m-2 d-flex flex-wrap">
+        <div class="border w-100 d-flex mb-2 mt-2 flex-wrap">
             <button class="btn btn-secondary m-1" @click="graying">黑白化</button>
+            <button class="btn m-1" :class="activateAddBox ? `btn-primary` : `btn-secondary`"
+                @click="addBoxEvent">绘制矩形框</button>
         </div>
-        <div class="d-block w-100 overflow-auto">
+        <div class="d-block w-100 overflow-auto border">
             <canvas ref="canvas" class="mw-100"></canvas>
         </div>
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onUnmounted, onMounted } from 'vue';
+import { debounce } from '@/utils/common';
+
 const canvas = ref(null);
 const btnDownload = ref(null);
 const btnInput = ref(null);
+const activateAddBox = ref(false);
+
 const handleFileChange = (e) => {
     const file = e.target.files[0];
     const context = canvas.value.getContext('2d');
@@ -41,6 +47,8 @@ const downloadCanvas = () => {
 }
 
 const graying = (e) => {
+    const option = window.confirm('此过程不可逆，是否继续');
+    if (!option) { return }
     const context = canvas.value.getContext('2d');
     const imgData = context.getImageData(0, 0, canvas.value.width, canvas.value.height);   //获取图片数据对象
     if (!imgData) {
@@ -53,6 +61,105 @@ const graying = (e) => {
         data[i] = data[i + 1] = data[i + 2] = average; //将每个像素点的色值重写
     }
     context.putImageData(new ImageData(data, canvas.value.width, canvas.value.height), 0, 0);  //将处理后的图像数据重写至Canvas画布，此时画布中图像变为黑白色
+}
+
+function offset(el) {
+    // get the client offset of the target element
+    let top = el.offsetTop
+    let left = el.offsetLeft
+    while (el.offsetParent) {
+        el = el.offsetParent
+        top += el.offsetTop
+        left += el.offsetLeft
+    }
+    return {
+        left: left,
+        top: top
+    }
+}
+
+function getEventPosition(ev) {
+    let x, y;
+    //注：使用这个函数，需要给Canvas元素的position设为absolute。
+    if (ev.layerX || ev.layerX == 0) {
+        x = ev.layerX;
+        y = ev.layerY;
+    } else if (ev.offsetX || ev.offsetX == 0) { // Opera
+        x = ev.offsetX;
+        y = ev.offsetY;
+    }
+    return { x: x, y: y };
+}
+
+function getEventPositionOffset(event, el) {
+    const { x: ex, y: ey } = getEventPosition(event);
+    const { left, top } = offset(el);
+    const [x, y] = [ex - left, ey - top];
+    return { x, y };
+}
+
+// get the ration of true imageWidh / canvas width
+const getCanvasRatio = () => {
+    // the border of canvas will effect canvas.value.clientWidth
+    const clientWidth = canvas.value.clientWidth;
+    const imageWidth = canvas.value.width;
+    const ratio = imageWidth / clientWidth;
+    return ratio;
+}
+
+// can't be defined in addBoxEvent
+// must keep canvasClickHandlerBox be the same Function Object
+let boxStartPoint = null;
+const canvasClickHandlerBox = (e) => {
+    // console.log(ratio);
+    const { x, y } = getEventPositionOffset(e, canvas.value);
+    // console.log(x, y);
+    if (boxStartPoint === null) {
+        boxStartPoint = { x, y };
+    } else {
+        // clean work
+        boxStartPoint = null;
+        boxEndPoint = null;
+    }
+}
+
+let boxEndPoint = null;
+const canvasMousemoveHandlerBox = debounce((e) => {
+    const context = canvas.value.getContext('2d');
+    if (boxStartPoint !== null) {
+        if (boxEndPoint !== null) {
+            // 清空之前的box
+            context.clearRect(boxStartPoint.x, boxStartPoint.y,
+                boxEndPoint.x - boxStartPoint.x, boxEndPoint.y - boxStartPoint.y);
+        }
+        const { x, y } = getEventPositionOffset(e, canvas.value);
+        boxEndPoint = { x, y };
+        // 绘制
+        const ratio = getCanvasRatio();
+        context.beginPath();
+        context.fillStyle = "#34c24e";
+        context.fillRect(boxStartPoint.x, boxStartPoint.y,
+            boxEndPoint.x - boxStartPoint.x, boxEndPoint.y - boxStartPoint.y);
+        context.closePath();
+        // console.log(`(${boxStartPoint.x}, ${boxStartPoint.y}) -> (${x}, ${y})`);
+    }
+
+}, 0);
+
+const addBoxEvent = () => {
+    activateAddBox.value = !activateAddBox.value;
+    if (activateAddBox.value) {
+        // console.log('添加事件');
+        // the only option removeEventListener() checks is 
+        // the capture/useCapture flag. Its value must match for removeEventListener() to match, 
+        // but the other values don't.
+        canvas.value.addEventListener('click', canvasClickHandlerBox, true);
+        canvas.value.addEventListener('mousemove', canvasMousemoveHandlerBox, true);
+    } else {
+        // console.log('卸载事件');
+        canvas.value.removeEventListener('click', canvasClickHandlerBox, true);
+        canvas.value.removeEventListener('mousemove', canvasMousemoveHandlerBox, true);
+    }
 }
 </script>
 
