88076cdd4557c39f5f4c9c74bdf6500803ebfb9c
weijl <1642063542@qq.com>
2023-02-11 14:13:02 +0800

    fix: 修正blog的menu路径

--- update_blogs.bat
+++ update_blogs.bat
@@ -1,5 +1,5 @@
 python .\public\assets\blogs\genlist.py
 git add ./public/assets
-git add ./src/components/blog/menu.json
-git commit -m "blogs: update contens"
+git add ./src/pages/blog/menu.json
+git commit -m "chore: update blog contens"
 git push origin dev
