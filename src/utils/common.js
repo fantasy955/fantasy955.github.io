c94aa93818067bbd8cf6523ca724a28bb7a9a67d
weijl <1642063542@qq.com>
2023-03-25 10:11:31 +0800

    fix: 修复防抖函数和节流函数this指向不正确的问题

--- src/utils/common.js
+++ src/utils/common.js
@@ -2,12 +2,15 @@
 // 一定时间内重复触发，只执行最后一次触发
 export const debounce = function (callback, timeout) {
     let job = undefined;
-    return (...args) => {
+    // 不能使用箭头函数（没有this指针）
+    return function (...args) {
+        let that = this;
+        // console.log(that);
         if (job !== undefined) {
             clearTimeout(job);
         }
         job = setTimeout(() => {
-            callback(...args);
+            callback.apply(that, args);
         }, timeout);
     }
 }
@@ -17,12 +20,13 @@ export const debounce = function (callback, timeout) {
 export const throttle = function (fn, delay) {
     let flag = true;
     return function (...args) {
+        let that = this;
         if (!flag) {
             return false;
         }
         // 工作时间，执行函数并且在间隔期内把状态位设为无效
         flag = false;
-        fn(...args);
+        fn.apply(that, ...args);
         setTimeout(() => {
             flag = true;
         }, delay)
