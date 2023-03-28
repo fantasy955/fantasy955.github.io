9f499d57be8fb8ab474ae53263eb8424fb61f503
weijl <1642063542@qq.com>
2023-03-28 10:37:52 +0800

    chore: 新增书籍

--- src/pages/book/book.list.js
+++ src/pages/book/book.list.js
@@ -10,7 +10,7 @@ export default [
         title: 'ECMScript入门',
         desc: '',
         file: '/assets/book/ECMScript入门.pdf',
-    }, 
+    },
     {
         post: '/assets/book/HTML5权威指南.png',
         title: 'HTML5权威指南',
@@ -22,10 +22,22 @@ export default [
         title: '前端面试宝典完整版最最最新',
         desc: '',
         file: '/assets/book/前端面试宝典完整版最最最新.pdf',
-    },{
+    }, {
         post: '/assets/book/CSS世界-张鑫旭.png',
         title: 'CSS世界-张鑫旭',
         desc: '',
         file: '/assets/book/CSS世界-张鑫旭.pdf',
+    },
+    {
+        post: '/assets/book/深入理解计算机系统.jpg',
+        title: '深入理解计算机系统',
+        desc: '',
+        file: '/assets/book/深入理解计算机系统.pdf',
+    },
+    {
+        post: '/assets/book/网络是怎样连接的_户根勤.png',
+        title: '网络是怎样连接的_户根勤',
+        desc: '',
+        file: '/assets/book/网络是怎样连接的_户根勤.pdf',
     }
 ]
